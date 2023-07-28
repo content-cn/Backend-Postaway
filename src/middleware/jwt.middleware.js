7871513cd9449412ce3d8508caee3ea601c27c0b
content-cn <87414959+content-cn@users.noreply.github.com>
2023-07-28 12:07:07 +0530

    Add files via upload

---
+++ src/middleware/jwt.middleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+const jwtAuth = (req, res, next) => {
+  const token = req.headers["authorization"];
+  if (!token) {
+    return res.status(401).send("Unauthorized");
+  }
+
+  try {
+    const payload = jwt.verify(token, "jwt_secret_key");
+    req.userId = payload.userId;
+  } catch (err) {
+    return res.status(401).send("Unauthorized");
+  }
+
+  next();
+};
+
+export default jwtAuth;
